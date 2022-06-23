fedaccb0086af7d437e6f88e3217dc2c87e60734
kevin <kevin@DESKTOP-MV2LI5V>
2022-06-23 01:41:17 -0600

    version  inicio

---
+++ src/java/BaseDeDatos/conexion.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package BaseDeDatos;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+public class conexion {
+   Connection conectar = null;
+     public Connection conexion(){
+         try{
+             Class.forName("com.mysql.jdbc.Driver");
+             conectar = DriverManager.getConnection("jdbc:mysql://localhost:3306/gestionestudiante","root","");
+             System.out.println("Conexion aceptada..");
+         }catch (Exception e){
+             System.out.println("Mensaje de error"+e);
+             JOptionPane.showMessageDialog(null,"Conexion Fallida.."+ e);
+         }
+        return conectar;
+     }
+
+   public static void main(String[] args){
+      conexion c = new conexion();
+      c.conexion();
+   }
+}
